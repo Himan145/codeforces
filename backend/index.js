6f39d3550fa75d6ecf320dbab84d25a9cdfaf8c7
Himan Biswas <135246264+Himan145@users.noreply.github.com>
2024-02-15 12:12:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,13 @@ const app=express();
 
 mongoose.connect('mongodb+srv://himanbiswas376:0FfrSr0bH1OM0V1k@cluster0.qbynmaw.mongodb.net/?retryWrites=true&w=majority');
 
-app.use(cors());
+app.use(cors(
+    {
+        origin:["https://crud-frontend-app.vercel.app"],
+        methods:["POST","PUT","DELETE","GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 app.post('/create',CreateProv);
